c5205bd10545b0b95e1739f3ed5c3899df0f4872
Vidhi-27 <76779913+Vidhi-27@users.noreply.github.com>
2025-08-28 19:45:12 +0530

    docling

--- hello.py
+++ hello.py
@@ -1,1 +1,17 @@
-print("hello World")
+# 2. Code
+from docling.datamodel.base_models import InputFormat
+from docling.datamodel.pipeline_options import PdfPipelineOptions
+from docling.document_converter import DocumentConverter, PdfFormatOption
+
+pipeline_options = PdfPipelineOptions(
+    artifacts_path="./my_models"          # <- folder that contains the repo
+)
+
+converter = DocumentConverter(
+    format_options={
+        InputFormat.PDF: PdfFormatOption(pipeline_options=pipeline_options)
+    }
+)
+
+result = converter.convert("input.pdf")
+print(result.document.export_to_markdown())
